ca948d706aec60b60acbbf56747842fa4e9e0f65
Siddhant Meshram <siddhantmeshram149@gmail.com>
2020-11-24 23:45:57 +0530

    Add files via upload

---
+++ Bubble Sort.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int a[100],n,i,j,t;
+printf("Enter the size of list<=100:");
+scanf("%d",&n);
+printf("Enter elements in the list:\n");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n-1;i++)
+{for(j=0;j<=(n-i)-1;j++)
+{if(a[j]>a[j+1])
+{t=a[j];
+a[j]=a[j+1];
+a[j+1]=t;
+}}}
+printf("Sorted list:");
+for(i=0;i<n;i++)
+printf("%d ",a[i]);
+}
+
